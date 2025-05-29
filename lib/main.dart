914f5018a924829aeb8dcd2fd25d9b57252206da
盧振興 <57373012+lohchanhin@users.noreply.github.com>
2025-05-30 02:50:54 +0800

    Init Flutter project skeleton

---
+++ lib/main.dart
@@ -0,0 +1,18 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const FitnessApp());
+}
+
+class FitnessApp extends StatelessWidget {
+  const FitnessApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Fitness Insight',
+      theme: ThemeData(primarySwatch: Colors.blue),
+      home: const Placeholder(),
+    );
+  }
+}
---
+++ lib/services/ai_service.dart
@@ -0,0 +1,6 @@
+class AIService {
+  Future<String> analyzeMeal(String imagePath) async {
+    // TODO: integrate with ML Kit
+    return '0 kcal';
+  }
+}
