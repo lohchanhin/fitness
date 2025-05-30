3699b6d6ac2cabd6cd0e45bbdc54d8603aabb8b3
盧振興 <57373012+lohchanhin@users.noreply.github.com>
2025-05-30 19:04:54 +0800

    feat: add auth screens and firebase auth

--- lib/services/auth_service.dart
+++ lib/services/auth_service.dart
@@ -1,6 +1,23 @@
+import 'package:firebase_auth/firebase_auth.dart';
+
 class AuthService {
+  final FirebaseAuth _auth = FirebaseAuth.instance;
+
   Future<bool> signIn(String email, String password) async {
-    // TODO: implement authentication logic
-    return true;
+    try {
+      await _auth.signInWithEmailAndPassword(email: email, password: password);
+      return true;
+    } on FirebaseAuthException {
+      return false;
+    }
+  }
+
+  Future<bool> signUp(String email, String password) async {
+    try {
+      await _auth.createUserWithEmailAndPassword(email: email, password: password);
+      return true;
+    } on FirebaseAuthException {
+      return false;
+    }
   }
 }
